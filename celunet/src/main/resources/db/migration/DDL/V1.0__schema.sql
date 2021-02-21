a061658a62bb15ddc1720d8f151d30089344744c
mauro <faber071493@hotmail.com>
2021-02-21 16:05:50 -0500

    Se agregan inserts de productos y el test de carga

--- celunet/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ celunet/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -34,7 +34,7 @@ create table compra (
  primary key (id)
 );
 
-Alter table compra ADD FOREIGN KEY (id_cliente) REFERENCES cliente(id);
+Alter table compra ADD FOREIGN KEY (id_cliente) REFERENCES cliente(id) on update cascade on delete cascade;
 
 create table detalle_compra(
  id int(11) not null auto_increment,
@@ -45,3 +45,8 @@ create table detalle_compra(
 );
 Alter table detalle_compra ADD FOREIGN KEY (id_compra) REFERENCES compra(id) on update cascade on delete cascade;
 Alter table detalle_compra ADD FOREIGN KEY (id_producto) REFERENCES producto(id);
+
+insert into producto(nombre, precio) values ('Xiaomi redmi note 8', 600000);
+insert into producto(nombre, precio) values ('Huawei P20 Lite', 500000);
+insert into producto(nombre, precio) values ('Iphone x', 3500000);
+insert into producto(nombre, precio) values ('Samgung s21', 750000);
